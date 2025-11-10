1a6b3393c127b497613f1480aee9c2f945d54f64
Guilherme Trajano <guilherme.trajano@urano.pro>
2025-11-10 19:21:00 -0300

    adiciona .gitignore, modelos de Review com status e avaliações, e requirements.txt

--- movies_review/reviews/models.py
+++ movies_review/reviews/models.py
@@ -1,3 +1,47 @@
+from tkinter import EXCEPTION
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Review(models.Model):
+    class Status(models.IntegerChoices):
+        """
+        dict para definir o status da review
+        1 - Publicado
+        0 - Rascunho
+        """
+
+        DRAFT = (0, "Rascunho")
+        PUBLISHED = (1, "Publicado")
+
+    class RatingChoices(models.IntegerChoices):
+        """
+        dict para definir a avaliação em estrelas
+        0 - 0 Estrelas
+        1 - 1 Estrela
+        2 - 2 Estrelas
+        3 - 3 Estrelas
+        4 - 4 Estrelas
+        5 - 5 Estrelas
+        """
+
+        BAD = (0, "Ruim")
+        POOR = (1, "Regular")
+        FAIR = (2, "Bom")
+        GOOD = (3, "Muito Bom")
+        EXCELLENT = (4, "Excelente")
+        EXCEPTIONAL = (5, "Excepcional")
+
+    title = models.CharField(max_length=200)
+    body = models.TextField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    status = models.IntegerField(choices=Status.choices, default=Status.DRAFT)
+    rating = models.IntegerField(
+        choices=RatingChoices.choices, default=RatingChoices.GOOD
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    published_at = models.DateTimeField(null=True, blank=True)
+
+    def __str__(self):
+        return self.title
